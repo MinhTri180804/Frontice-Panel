e9e53d7777fb93ebf530298be54a35d145a3b232
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-12-11 12:52:35 +0700

    feat(page): completed notfound page

--- src/pages/ErrorPage/NotFound/NotFound.tsx
+++ src/pages/ErrorPage/NotFound/NotFound.tsx
@@ -1,10 +1,24 @@
+import { Button, Result } from "antd";
 import { FC } from "react";
+import { useNavigate } from "react-router";
 
 const NotFoundPage: FC = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div>this is notfound page</div>
-    </div>
+    <Result
+      status={"404"}
+      title="404 NOT FOUND"
+      subTitle="Trang bạn tìm kiếm hiện không tồn tại"
+      extra={
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => navigate("/")}
+        >
+          Quay về trang chủ
+        </Button>
+      }
+    />
   );
 };
 
